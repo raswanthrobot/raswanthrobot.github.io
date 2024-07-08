1071228f9c49102a1fff6e435eab975b5b5d616a
Nitish Saravanan <73633817+Raswanth-22@users.noreply.github.com>
2024-07-08 12:42:48 +0530

    COMMIT

--- raswanthrobot/src/app/app-routing.module.ts
+++ raswanthrobot/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResearchComponent } from './research/research.component';
+import { PublicationsComponent } from './publications/publications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'projects',component:ProjectsComponent},
+ {path:'research',component:ResearchComponent},
+ {path:'publications',component:PublicationsComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- raswanthrobot/src/app/app.module.ts
+++ raswanthrobot/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResearchComponent } from './research/research.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PublicationsComponent } from './publications/publications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResearchComponent,
+    ProjectsComponent,
+    PublicationsComponent
   ],
   imports: [
     BrowserModule,
